7c8b0ccd85d96104e62d5152997eacf1e75f27c3
m-krotlinski <m.krotlinski@unionsmarket.com>
2014-02-05 13:54:07 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Doudou::Application.config.secret_key_base = 'b97dec6ac212bff83247f93cfa962b2b71945fb773cd5dfc36a7c670f2d82446b675bcd773f869555271fbb19b4b07a23e76dcee92fd19ed02b07b64f90caf25'
